0e2cb3ddf3609b1df4cc390e988dda32c405bc3e
soda-flavour <axa8380@gmail.com>
2020-11-09 20:05:11 +0900

    refactor: Practice vue3 with typescript

---
+++ web-apis/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav-bar />
+  <router-view/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import NavBar from '@/components/NavBar';
+
+export default defineComponent({
+  components:{
+    NavBar
+  }
+});
+
+</script>
+
+<style lang="scss">
+
+</style>
