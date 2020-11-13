b7ddaf013eadf66b0b0b90d1ce4136f5c55b265f
soda-flavour <axa8380@gmail.com>
2020-11-13 19:32:08 +0900

    refactor: Update Exception type for error handle

--- backend/src/api/lib/HttpException.ts
+++ backend/src/api/lib/HttpException.ts
@@ -1,11 +1,13 @@
+import { errorMsg } from "../constant/errorMsg";
+
 class HttpException extends Error {
   status: number;
   message: string;
   errorCode: string;
-  constructor(status: number, message: string, errorCode: string) {
+  constructor(status: number, errorCode: string, message: string = "") {
     super(message);
     this.status = status;
-    this.message = message;
+    this.message = errorMsg[errorCode];
     this.errorCode = errorCode;
   }
 }
